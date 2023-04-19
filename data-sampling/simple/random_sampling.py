d9a6e4f20838628f5d1872fcfe7b2d17b1a20ae3
Paulo Reis <paulo.reis@kobe.io>
2023-04-19 14:51:08 -0300

    refactor(random_sampling): removed unecessary for and samples

--- data-sampling/simple/random_sampling.py
+++ data-sampling/simple/random_sampling.py
@@ -2,12 +2,5 @@ from random import sample
 
 population = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
-sample1 = sample(population, 4)
-sample2 = sample(population, 4)
-sample3 = sample(population, 4)
-sample4 = sample(population, 4)
-
-for i in range(4):
-    print(sample(population, 4))
-
-
+# gets 4 random numbers from population
+print(sample(population, 4))
